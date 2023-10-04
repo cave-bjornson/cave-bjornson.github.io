20d2df9ec9912ae5b37777c44190c24c0a83f06a
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-10-04 19:00:26 +0200

    chore: commit before testing another remix plugin

--- src/routes/__root/blog.tsx
+++ src/routes/__root/blog.tsx
@@ -1,5 +1,3 @@
-import { Outlet } from "react-router-dom";
-
 export const Component = () => {
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import RemixRouter from "vite-plugin-remix-router";
+import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), RemixRouter()],
+  plugins: [
+    { enforce: "pre", ...mdx() },
+    react(),
+    RemixRouter({ extensions: ["jsx", "tsx"] }),
+  ],
 });
