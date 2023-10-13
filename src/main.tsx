2453de85b41ab90bb02302f8e47512770378f53e
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-10-13 16:11:22 +0200

    refactor: change to hashrouter
    Browser router does not work with paths on github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "react-responsive-modal/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
   // </React.StrictMode>
 );
